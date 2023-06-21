04021b4ff621d2c37684b69f63fa8cbf96288376
Lucas Silva <gaviao.lucas@gmail.com>
2023-06-21 09:13:57 +0100

    commit before exercise

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import '../css/Footer.css'
+
+
+function Footer(){
+    return (<div className="Footer">
+    <footer> This is my Footer</footer>
+    </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import '../css/Header.css'
+
+export const Header = () => {
+    return( 
+    <div className="Header">
+    <h1> This is my header </h1>
+    </div>
+    )
+}
